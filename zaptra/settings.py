cb50e192dd4af9bf1c11513f1e1973a4de024343
fabioost <fabioost@bol.com.br>
2020-01-23 21:11:40 -0300

    add alowed hosts 192.168.1.11.xip.io

--- zaptra/settings.py
+++ zaptra/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'r2hr#58-+1_ty-se_d6j#9=^@v+b59ls%b3@_%8_4m5((rbj*l'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['zaptra.herokuapp.com','192.168.1.11','127.0.0.1']
+ALLOWED_HOSTS = ['zaptra.herokuapp.com','192.168.1.11','127.0.0.1',192.168.1.11.xip.io]
 
 
 # Application definition
